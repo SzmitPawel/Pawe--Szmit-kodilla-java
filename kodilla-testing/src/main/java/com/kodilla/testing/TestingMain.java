6beaaf2e1c038e255de3613566adaa30a2ddfd75
SzmitPaweł <szmitp@oriax.eu>
2022-12-07 20:22:45 +0100

    6.2 Added first test in Testingmain

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,28 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        Calculator calculator = new Calculator();
+
+        int result = calculator.add(2,2);
+
+        if (result == 4) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("ERROR!");
+        }
+
+        result = calculator.subtract(5,2);
+
+        if (result == 3) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("ERROR!");
+        }
+
     }
 }
