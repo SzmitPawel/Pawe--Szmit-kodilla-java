4e765feee0216114d850c96a93f0fb966dc33d97
SzmitPaweł <szmitp@oriax.eu>
2022-12-08 19:03:39 +0100

    6.2 Push again Calculator class

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing.forum;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Forum Test Suite")
+public class ForumTestSuite {
+
+
+
+}
